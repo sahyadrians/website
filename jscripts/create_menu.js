0fa3a0622456382cd71923a7f138b307c4489495
Sahyadrians <sahyadrians@gmail.com>
2014-03-17 04:07:28 +0530

    Automate menu create
    Create a javascript function that creates the menu. This is done to
    avoid repeated coding.

---
+++ jscripts/create_menu.js
@@ -0,0 +1,44 @@
+<!--
+
+function create_menu(pageIdx) {
+
+	if (pageIdx == 1) {
+		document.write("<li class=\"current_page_item\"><a href=\"./index.html\" accesskey=\"1\" title=\"\">Homepage</a></li>");
+	} else {
+		document.write("<li><a href=\"./index.html\" accesskey=\"1\" title=\"\">Homepage</a></li>");
+	}
+		
+	if (pageIdx == 2) {
+		document.write("<li class=\"current_page_item\"><a href=\"#\" accesskey=\"2\" title=\"\">Events</a></li>");
+	} else {
+		document.write("<li><a href=\"#\" accesskey=\"2\" title=\"\">Events</a></li>");
+	}
+	
+	if (pageIdx == 3) {
+		document.write("<li class=\"current_page_item\"><a href=\"#\" accesskey=\"3\" title=\"\">Alumni</a></li>");
+	} else {
+		document.write("<li><a href=\"#\" accesskey=\"3\" title=\"\">Alumni</a></li>");
+	}
+	
+	if (pageIdx == 4) {
+		document.write("<li class=\"current_page_item\"><a href=\"#\" accesskey=\"4\" title=\"\">Forum</a></li>");
+	} else {
+		document.write("<li><a href=\"#\" accesskey=\"4\" title=\"\">Forum</a></li>");
+	}
+	
+	if (pageIdx == 5 ) {
+		document.write("<li class=\"current_page_item\"><a href=\"./gallery.html\" accesskey=\"5\" title=\"\">Gallery</a></li>");
+	} else {
+		document.write("<li><a href=\"./gallery.html\" accesskey=\"5\" title=\"\">Gallery</a></li>");
+	}
+	
+	if (pageIdx == 6 ) {
+		document.write("<li class=\"current_page_item\"><a href=\"#\" accesskey=\"6\" title=\"\">Give Back</a></li>");
+	} else {
+		document.write("<li><a href=\"#\" accesskey=\"6\" title=\"\">Give Back</a></li>");
+	}
+	
+	return true;
+}
+
+-->
