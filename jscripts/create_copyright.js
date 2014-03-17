36882497180675e9b015ffeac54c160e7eb444cf
Sahyadrians <sahyadrians@gmail.com>
2014-03-17 16:17:30 +0530

    Automate copyright notice creation
    Create a script that will create the copyright div at bottom of the page

---
+++ jscripts/create_copyright.js
@@ -0,0 +1,10 @@
+<!--
+
+function create_copyright() {
+
+	document.write("<div id=\"copyright\" class=\"container\">");
+	document.write("<p>Copyright (c) 2014 Sahyadrians.com. All rights reserved. Design by <a href=\"http://www.freecsstemplates.org/\" rel=\"nofollow\">FreeCSSTemplates.org</a>.</p>");
+	document.write("</div>");
+
+}
+-->
