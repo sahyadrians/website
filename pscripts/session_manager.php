b616a3ed0d0b711a0a1dedd541d7307641b94df4
Sahyadrians <sahyadrians@gmail.com>
2014-03-30 23:47:15 +0530

    Changed line endings
    Changed line endings to UNIX

--- pscripts/session_manager.php
+++ pscripts/session_manager.php
@@ -26,47 +26,6 @@
 		echo "Auth set to " . $_SESSION['auth'] . " by GET method";
 	}
 
-/*
-	
-	// To get a session variable, one should give the parameter varName
-	if( isset( $_POST['varNameGet'] ) ) {
-		$varName = $_POST['varNameGet'];
-		if( isset( $_SESSION[varName] ) )	{ echo $_SESSION[$varName]; }
-		else								{ echo ""; }
-	}
-	elseif( isset( $_GET['varNameGet'] ) ) {
-		$varName = $_GET['varNameGet'];
-		if( isset( $_SESSION[$varName] ) )	{ echo $_SESSION[$varName]; }
-		else								{ echo ""; }
-	}
-	
-	// To get a set a variable, one should give the parameter varNameSet and val
-	elseif( isset( $_POST['varNameSet'] ) and isset( $_POST['val'] ) ) {
-		$varName = $_POST['varName'];
-		$_SESSION[$varName] = $_POST['val'];
-		echo $_POST['val'];
-	}
-	elseif( isset( $_GET['varNameSet'] ) and isset( $_GET['val'] ) ) {
-		$varName = $_GET['varName'];
-		$_SESSION[$varName] = $_GET['val'];
-		echo $_GET['val'];
-	}
-	
-	// To check if a session exists, the script is called with parameter check=1
-	elseif( isset( $_POST['checkToken'] ) or isset($_GET['checkToken'] ) ) {
-		if( isset( $_SESSION['token'] ) )	{ echo 1; }
-		else								{ echo 0; }
-	}
-
-	elseif( isset( $_POST['token'] ) ) {
-		$_SESSION['token'] = $_POST['token'];
-		echo "Token (POST) - " . $_SESSION['token'];
-	}
-	elseif( isset( $_GET['token'] ) ) {
-		$_SESSION['token'] = $_GET['token'];
-		echo "Token (GET) - " . $_SESSION['token'];
-	}
-*/
 	else {
 		session_destroy();
 		echo "Session destroyed";
