f7d9f4e340ad1879e0504bcef2c4c6c0c07d6966
Sahyadrians <sahyadrians@gmail.com>
2014-04-05 15:42:32 +0530

    Simplify session manager
    Adding the session manager file to the commit!
    (the previous commit is incomplete...)

--- pscripts/session_manager.php
+++ pscripts/session_manager.php
@@ -11,18 +11,11 @@
 	
 	// Checking token status
 	elseif( isset( $_POST['getTokenStatus'] ) or isset( $_GET['getTokenStatus'] ) ) {
-		if( isset( $_SESSION['token'] ) ) 	{ echo 1; }
-		else								{ echo 0; }
-	}
-	
-	// Set the log-in status to whatever value
-	elseif( isset( $_POST['setLogin'] ) ) {
-		$_SESSION['auth'] = $_POST['setLogin'];
-		echo "Auth set to " . $_SESSION['auth'] . " by POST method";
-	}
-	elseif( isset( $_GET['setLogin'] ) ) {
-		$_SESSION['auth'] = $_GET['setLogin'];
-		echo "Auth set to " . $_SESSION['auth'] . " by GET method";
+		if( isset( $_SESSION['token'] ) ) 	{ 
+			$_SESSION['auth']		= true;
+			echo $_SESSION['auth'];
+		}
+		else { echo 0; }
 	}
 
 	else {
