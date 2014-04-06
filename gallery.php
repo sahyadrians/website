d3cb23466865cb3b41898c5a1406bc6b498aa74b
Sahyadrians <sahyadrians@gmail.com>
2014-04-06 10:50:51 +0530

    Gallery log-in sensitive
    Make the gallery log-in sensitive as well

--- gallery.php
+++ gallery.php
@@ -1,4 +1,7 @@
-<?php session_start(); ?>
+<?php 
+	// List down the required libraries/functions
+	require_once './pscripts/create_menu.php';
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
@@ -15,7 +18,7 @@ Released   : 20140216
 -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-<?php 
+<?php
 	include '../db_access/gallery_main.php';
 	
 	// get the required id
@@ -36,7 +39,11 @@ Released   : 20140216
 ?>
 
 <head>
+	<!-- Hide the sign-in message -->
+	<style>iframe[src^="https://apis.google.com/u/0/_/widget/oauthflow/toast"] {display: none;}</style>
+	
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
 	<title>Sahyadrians Gallery</title>
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
@@ -46,11 +53,16 @@ Released   : 20140216
 	<link href="style/fonts.css" 	rel="stylesheet" type="text/css" media="all" />
 
 <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="https://apis.google.com/js/client:plusone.js" type="text/javascript"></script>
+	<script src="https://apis.google.com/js/client:platform.js" type="text/javascript"></script>
 
-	<script src="jscripts/check_login.js"></script>
-	<script src="jscripts/create_menu.js"></script>
-	<script src="jscripts/create_copyright.js"></script>
-
+	<!-- Code with Google sign-in data -->
+	<meta name="google-signin-callback" content="signinCallback" />
+	<script type="text/javascript"	src="./jscripts/google-signin.js"/>
+	
+	<!-- page won't load if I don't include this! god only knows why! -->
+	<script src="./jscripts/create_copyright.js"></script>
 </head>
 <body>
 	<div id="wrapper">
@@ -59,9 +71,7 @@ Released   : 20140216
 				<h1><a href="#">Gallery</a></h1>
 			</div>
 			<div id="menu">
-				<ul>
-					<script>create_menu(5,1)</script>
-				</ul>
+				<?php create_menu(5) ?>
 			</div>
 		</div>
 		
@@ -85,8 +95,20 @@ Released   : 20140216
 			</div>
 		</div>
 	</div>
-	
-	<script>create_copyright()</script>
+
+	<?php include './pscripts/create_copyright.php';?>
+
+	<div id="signinButton-hack">
+		<span class="g-signin"
+			data-scope="https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read"
+			data-accesstype="offline"
+			data-clientid="427000476887.apps.googleusercontent.com"
+			data-redirecturi="postmessage"
+			data-requestvisibleactions='http://schemas.google.com/AddActivity'
+			data-cookiepolicy="single_host_origin"
+			data-callback="signInCallback">
+		</span>
+	</div>
 </body>
 
 </html>
